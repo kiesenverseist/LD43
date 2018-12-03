a749006d10888c2281b8909d23ce13b5b6e2cd55
XTsaid <XTsaid@gmail.com>
2018-12-03 21:19:45 +1100

    added 2 cards/ added float thingy

--- cards/base_card.gd
+++ cards/base_card.gd
@@ -9,26 +9,26 @@ export(int, 1, 3) var tier
 export var benifits = {
 	description = "",
 	instant = {
-		human = 0,
-		research = 0,
-		money = 0
+		human = 0.0,
+		research = 0.0,
+		money = 0.0
 	},
 	generation = {
-		human = 0,
-		research = 0,
-		money = 0
+		human = 0.0,
+		research = 0.0,
+		money = 0.0
 	}
 }
 export var detriments = {
 	description = "",
 	instant = {
-		human = 0,
-		research = 0,
-		money = 0
+		human = 0.0,
+		research = 0.0,
+		money = 0.0
 	},
 	generation = {
-		human = 0,
-		research = 0,
-		money = 0
+		human = 0.0,
+		research = 0.0,
+		money = 0.0
 	}
 }
